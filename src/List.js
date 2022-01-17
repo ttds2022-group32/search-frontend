7da02e7a41a8811b50ad63ba46585689b1327d93
Altariarite <yuanting.mao@gmail.com>
2022-01-17 22:00:20 +0000

    changed list style

--- src/List.js
+++ src/List.js
@@ -5,9 +5,9 @@ import React, { useState } from 'react'
  */
 const featuredImages = ['img-1', 'img-2', 'img-3'];
 const List = () => {
-  return <div class="p-2 mx-auto rounded-full bg-white shadow flex items-center space-x-4">
+  return <div class="p-2  max-w-3xl my-4 bg-white shadow-lg flex items-center space-x-4">
     {/*  */}
-    <div class="shrink-0">
+    <div class="shrink-0 mx-2">
       <img class="h-12 w-12" src="../public/logo512.png" alt="ChitChat Logo" />
     </div>
     <div>
