2d8b49531d9b5e1272c844a72712b1800eb8641b
Altariarite <yuanting.mao@gmail.com>
2022-01-17 23:22:28 +0000

    Changed List to contain random image

--- src/List.js
+++ src/List.js
@@ -9,7 +9,7 @@ const List = (props) => {
   return <div className="p-2  max-w-3xl my-4 bg-white shadow-lg flex items-center space-x-4">
     {/*  */}
     <div className="shrink-0 mx-2">
-      <img className="h-12 w-12" src="../public/logo512.png" alt="ChitChat Logo" />
+      <img className="h-12 w-12" src="https://source.unsplash.com/random" alt="ChitChat Logo" />
     </div>
     <div>
       <div className="text-xl font-medium text-black">{props.results}</div>
