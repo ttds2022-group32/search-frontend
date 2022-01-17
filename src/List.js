898cb782eedd96bf0bc6e2b0b52fa155527375eb
Altariarite <yuanting.mao@gmail.com>
2022-01-17 23:06:00 +0000

    install axios and render list

--- src/List.js
+++ src/List.js
@@ -4,15 +4,16 @@ import React, { useState } from 'react'
  * @function List
  */
 const featuredImages = ['img-1', 'img-2', 'img-3'];
-const List = () => {
-  return <div class="p-2  max-w-3xl my-4 bg-white shadow-lg flex items-center space-x-4">
+const List = (props) => {
+  console.log(props)
+  return <div className="p-2  max-w-3xl my-4 bg-white shadow-lg flex items-center space-x-4">
     {/*  */}
-    <div class="shrink-0 mx-2">
-      <img class="h-12 w-12" src="../public/logo512.png" alt="ChitChat Logo" />
+    <div className="shrink-0 mx-2">
+      <img className="h-12 w-12" src="../public/logo512.png" alt="ChitChat Logo" />
     </div>
     <div>
-      <div class="text-xl font-medium text-black">A Wizard of the EarthSea</div>
-      <p class="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam officia labore culpa voluptas sequi possimus pariatur assumenda laudantium rem nostrum cum corporis autem distinctio, molestiae accusamus temporibus? Aliquam, architecto autem?</p>
+      <div className="text-xl font-medium text-black">{props.results}</div>
+      <p className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam officia labore culpa voluptas sequi possimus pariatur assumenda laudantium rem nostrum cum corporis autem distinctio, molestiae accusamus temporibus? Aliquam, architecto autem?</p>
     </div>
   </div>
 }
--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,45 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import List from './List';
+
 /**
  * @function Search
  */
 
-const Search = () => {
+const Search = (props) => {
+    const [searchTerms, setSearchTerms] = useState('')
+    const [results, setResults] = useState()
+
+    const addSearch = (event) => {
+        event.preventDefault()
+        console.log('button clicked', searchTerms)
+        axios
+            .post("http://localhost:8080/", searchTerms)
+            .then(response => {
+                console.log(response)
+                setResults(response.data)
+            })
+
+    }
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setSearchTerms(event.target.value)
+    }
+
     return (
         <div className='bg-zinc-200 h-[100vh] w-[100vw]'>
             <div className='absolute inset-60'>
-                <input className='w-[400px] h-[50px] rounded-l-full shadow-lg shadow-zinc-300 outline-none
+                <input value={searchTerms} onChange={handleChange} className='w-[400px] h-[50px] rounded-l-full shadow-lg shadow-zinc-300 outline-none
                 font-thin text-xl px-4' placeholder='book name; author; published year...'></input>
-                <button className='h-[50px] w-[100px] bg-zinc-400 rounded-r-full text-xl font-thin
+                <button onClick={addSearch} className='h-[50px] w-[100px] bg-zinc-400 rounded-r-full text-xl font-thin
                 shadow-lg shadow-zinc-300 text-white hover:bg-zinc-500'>Search</button>
-            </div>    
+
+                {results && <List results={results} />}
+            </div>
+
         </div>
+
     )
 }
 
